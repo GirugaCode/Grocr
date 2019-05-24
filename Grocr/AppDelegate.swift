f2096591d877e1bbebe1fa580e8b20ab2ee05759
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-05-24 00:30:51 -0700

    [update] finished tutorial, Enabled offline capabilities

--- Grocr/AppDelegate.swift
+++ Grocr/AppDelegate.swift
@@ -10,6 +10,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        // Enable the app to work offline and reconnect data once wifi is found
+        Database.database().isPersistenceEnabled = true
         return true
     }
 }
